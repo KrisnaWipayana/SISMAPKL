ff791c3940545bfc0e0d4400bb489eb7f80bcb27
trip2trip <krisnawipayana1974@gmail.com>
2025-01-15 17:23:16 +0700

    Front-end structure

---
+++ Vue/SISMAPKL/src/components/TestVue.vue
@@ -0,0 +1,16 @@
+<!-- ISI SCRIPT JS -->
+<script setup>
+console.info("hello");
+</script>
+
+<!-- ISI TAMPILAN BASIC HTML -->
+<template>
+<h1 class="hello">Halo test 1 2 3</h1>
+</template>
+
+<!-- ISI STYLING CSS -->
+<style scoped>
+.hello {
+  color: blue;
+}
+</style>
---
+++ Vue/SISMAPKL/src/test-hello.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+
+import TestVue from "./components/TestVue.vue";
+
+createApp(TestVue).mount("#app");
