54462b0b02106720bafe0eb125f8f8f0d31db517
trip2trip <krisnawipayana1974@gmail.com>
2025-01-16 13:29:00 +0700

    go structure (fiber+gorm)

---
+++ Go/SISMAPKL/go.mod
@@ -0,0 +1,3 @@
+module sismapkl
+
+go 1.23.0
--- Vue/SISMAPKL/src/components/TestVue.vue
+++ Vue/SISMAPKL/src/components/TestVue.vue
@@ -5,12 +5,43 @@ console.info("hello");
 
 <!-- ISI TAMPILAN BASIC HTML -->
 <template>
-<h1 class="hello">Halo test 1 2 3</h1>
+<h1 class="title">Halaman login</h1>
+
+<div class="card">
+  <div class="div-card-body">
+    <h5 class="card-title">Halo</h5>
+      <p class="card-text">Email</p>
+  </div>
+</div>
 </template>
 
 <!-- ISI STYLING CSS -->
 <style scoped>
-.hello {
-  color: blue;
+.title {
+  color: black;
+  text-align:center ; 
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+.card-title {
+  text-align: center;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+.card {
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 70vh;
+  align-items: center;
+}
+
+.card-body {
+  padding: 20px;
 }
 </style>
